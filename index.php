3c540833318c8bf643a4ce9790ad1a063127c382
Charles Owusu <kofidesign932@gmail.com>
2018-03-16 21:54:38 +0000

    add top banner image to home

--- index.php
+++ index.php
@@ -15,14 +15,14 @@
     <!-- custom -->
 
     <!-- google fonts used -->
-    <link href="https://fonts.googleapis.com/css?family=Cookie|Lato" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <!-- google fonts used -->
 
 
   </head>
   <body>
 
-   <nav class="navbar navbar-expand-lg navbar-light sleight-headbg">
+   <nav class="navbar navbar-expand-lg navbar-light sleight-headbg fixed-top">
     <div class="container">
  <a href="index.html"><img src="img/sleight.png" class="img-fluid navbar-brand" width="104" height="68"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,29 +32,33 @@
   <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Home </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">How It Works</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
+
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link" href="#">Login</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="btn btn-sleight" href="login.html">Signup</a>
       </li>
     </ul>
   </div>
   </div>
 </nav>
+
+<!-- home top image -->
+<section class="sleight-back">
+  <div class="text-center homt">
+			<h1 class="display-2">LUXURY DEFINED</h1>
+			<h1>AT</h1>
+			<h1>ACCRA CITY HOTEL</h1>
+			</div>
+</section>
+<!-- home top image -->
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
