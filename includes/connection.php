6ecfebf42ab5f6bce5264ac2f7c4af634127d978
Nathaniel <NathanielNat@users.noreply.github.com>
2018-03-18 15:57:24 +0000

    Index working

---
+++ includes/connection.php
@@ -0,0 +1,14 @@
+<?php
+
+// server name
+// user name
+// password
+//db name
+$dbServername = "localhost";
+$dbName  = "project_php";
+$dbUsername = "root";
+$dbPassword  ="";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+
+ ?>
