98f09a120768fe6be6e9034e4235975ac744d8fa
Charles Owusu <kofidesign932@gmail.com>
2018-03-19 18:58:49 +0000

    Re arrainge navigation on login and signup

--- signup.php
+++ signup.php
@@ -27,34 +27,33 @@
   </head>
   <body>
 
-   <nav class="navbar navbar-expand-lg navbar-light sleight-headbg fixed-top">
-    <div class="container">
- <a href="index.html"><img src="img/sleight.png" class="img-fluid navbar-brand" width="104" height="68"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <nav class="navbar navbar-expand-lg navbar-light sleight-headbg fixed-top">
+     <div class="container">
+  <a href="index.html"><img src="img/sleight.png" class="img-fluid navbar-brand" width="104" height="68"></a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
 
-  <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">How It Works</a>
-      </li>
+   <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+     <ul class="navbar-nav">
+       <li class="nav-item active">
+         <a class="nav-link" href="#">Home </a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">How It Works</a>
+       </li>
 
-      <li class="nav-item">
-        <a class="btn btn-sleight" href="login.php">Login</a>
+       <li class="nav-item">
+         <a class="nav-link" href="#">Login</a>
+       </li>
 
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" href="#">Signup</a>
-      </li>
-    </ul>
-  </div>
-  </div>
-</nav>
+       <li class="nav-item">
+         <a class="btn btn-sleight" href="login.html">Signup</a>
+       </li>
+     </ul>
+   </div>
+   </div>
+ </nav>
 
 
 <section class="login-head">
